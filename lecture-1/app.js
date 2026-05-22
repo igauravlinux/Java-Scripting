2d33efb59b8e3fc82afd07ff2b86b4bca0cbe095
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-05-22 23:18:21 +0530

    update

--- lecture-1/app.js
+++ lecture-1/app.js
@@ -1,12 +1,17 @@
-let name = document.querySelector("h1");
-let btn = document.querySelector("button");
+// Variables
 
-name.addEventListener("mouseover",()=>{
-    name.style.color = 'orange';
-    name.style.backgroundColor = 'darkblue';
-})
+fullName = "Gaurav kumar";
+age = 23;
+price = 99.90;
 
-btn.addEventListener("click",()=>{
-    btn.style.color = 'orange';
-    btn.style.backgroundColor = 'darkblue';
-})
+x = null;
+y = undefined;
+isDrunk = true;
+
+console.log(fullName);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isDrunk);
+
