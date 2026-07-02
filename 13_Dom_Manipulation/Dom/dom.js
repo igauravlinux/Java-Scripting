dc7a3d49c7006402aa24a438a176773c0c92ae91
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-07-02 23:55:55 +0530

    Dom Manupulation

---
+++ 13_Dom_Manipulation/Dom/dom.js
@@ -0,0 +1,26 @@
+
+// getElementById()
+
+let message = document.getElementById("message");
+console.log(message);
+
+// getElementsByName()
+
+let btn = document.getElementsByName("language");
+console.log(btn);
+
+// getElementsByTagName()
+
+let heading = document.getElementsByTagName("h1");
+console.log(heading);
+
+// getElementsByClassName() 
+
+let cont = document.getElementById("container");
+// container ke andar ke class ko pakrega
+let msg1 = cont.getElementsByClassName("msg");
+console.log(msg1); 
+
+let msg = document.getElementsByClassName("msg");
+console.log(msg);
+
