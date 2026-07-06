5c3255f6635e2c8d0bcd831b8ebee509dee30a50
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-07-07 00:58:08 +0530

    Dom Manipulation

---
+++ 13_Dom_Manipulation/ContentChanging/app.js
@@ -0,0 +1,38 @@
+
+
+// innerText
+// let title1 = document.getElementById("titleTwo");
+let title2 = document.getElementsByClassName("titleOne");
+
+// title2[0].innerText = "Rahul"
+// title2[1].innerText = "Rahul"
+
+// title2[0].innerText = "hey i am Soni";
+// title1.innerText = "Rahul kumar"
+
+
+
+//  <!-- textContent -->
+// let name = document.getElementById("name");
+
+// console.log(name.innerText);
+// console.log(name.textContent);
+
+
+// innerHTML
+
+// let car = document.getElementById("car");
+
+// console.log(car); //reading
+
+// car.innerHTML = "<p>hey this is a bike</p>";
+
+
+
+
+// outerHTML
+
+// let card = document.getElementById("card");
+
+// console.log(card.outerHTML);
+
