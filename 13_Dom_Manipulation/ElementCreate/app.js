8cbae67d21514b24d8d758f355a27fca16dad637
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-07-10 23:51:00 +0530

    Dom Manipulation Inserting Elements

---
+++ 13_Dom_Manipulation/ElementCreate/app.js
@@ -0,0 +1,26 @@
+let taskBar = document.getElementById("taskBar");
+let addBtn = document.getElementById("addBtn");
+let listItems = document.getElementById("listItems");
+
+
+addBtn.addEventListener("click",addTask);
+
+
+function addTask(){
+
+    let input = taskBar.value.trim();
+
+    if(input === ""){
+        alert("please add a task");
+        return;
+    }
+
+
+    // creating li element and adding in ul 
+    let list = document.createElement("li");
+    list.innerText = input;
+    listItems.append(list);
+
+    taskBar.value = "";
+
+}
