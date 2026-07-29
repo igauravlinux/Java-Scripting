983422eb713f2bcf2b5c63f73c778c74681e9fb3
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-07-29 20:37:07 +0530

    Async-await

--- 22_AsyncAwait/async-await.js
+++ 22_AsyncAwait/async-await.js
@@ -1,3 +1,42 @@
+let title = document.getElementById("title");
+
+
+// How to use aync-await
+async function fetchData(){
+
+    try{
+
+        // wait for response
+        let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+        // Convert response to JSON
+        let data = await response.json();
+
+        console.log(data);
+        title.innerText = data.title;
+    }
+
+    catch(error){
+        console.log("Error: ",error);
+    }
+
+}
+
+
+fetchData();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // async function fetchData(){
 //     try{
