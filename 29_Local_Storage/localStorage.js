50f77d71860534908c9498eef8b7c1ac7ced617d
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-08-06 23:21:08 +0530

    Local Storage

---
+++ 29_Local_Storage/localStorage.js
@@ -0,0 +1,25 @@
+
+// Set Item
+localStorage.setItem("username","Gaurav Kumar");
+localStorage.setItem("age","23");
+localStorage.setItem("city","Patna");
+localStorage.setItem("phoneNum","88234562533");
+
+// Get Item
+// username
+let userName = localStorage.getItem("username");
+console.log(userName);
+// city
+let city = localStorage.getItem("city");
+console.log(city);
+// age
+let age = localStorage.getItem("age");
+console.log(age);
+
+
+// Remove One Item
+localStorage.removeItem("phoneNum");
+localStorage.removeItem("city");
+
+// clear All
+localStorage.clear();
